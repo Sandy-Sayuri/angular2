cf7a19d2c4f0dc21beadfd92b64ee1e2c80b3f2d
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-02-08 09:41:35 -0300

    teste feito

---
+++ HellloWord/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'meu-primero-component',
+    template:`
+    <p> Meu primeiro component com Angula 2 <p>
+    `
+})
+ export class MeuPrimeiroComponent{
+
+}
