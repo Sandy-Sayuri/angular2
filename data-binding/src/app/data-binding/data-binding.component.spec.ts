5e5982b167dbeab893cbc0e71a91b073d08e3599
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-02-17 14:58:55 -0300

    estudo

--- data-binding/src/app/data-binding/data-binding.component.spec.ts
+++ data-binding/src/app/data-binding/data-binding.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DataBindingComponent } from './data-binding.component';
 
 describe('DataBindingComponent', () => {
--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,9 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
+  
+
 })
 export class DataBindingComponent implements OnInit {
+    valorAtual:string='';
+    valorSalvo;
     url:string='https://www.cursoemvideo.com/';
     cursoAngular:boolean=true;
     url1='http://lorempixel.com.br/400/200';
@@ -14,6 +18,17 @@ export class DataBindingComponent implements OnInit {
     }
     getCurtirCurso(){
       return true;
+    }
+    botaoClicado(){
+      alert('botão Clicado')
+    }
+    onKeyUp(evento:KeyboardEvent){
+      console.log((<HTMLInputElement>evento.target).value);
+      this.valorAtual=(<HTMLInputElement>evento.target).value;
+
+    }
+    salvarValor(valor:String){
+      this.valorSalvo=valor;
     }
   constructor() { }
 
